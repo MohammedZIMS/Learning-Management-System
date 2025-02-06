77142bd0a440dc9902953ac6b29d53d86bf9708a
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-06 14:33:30 +0600

    Update Store.js

--- Client/src/app/Store.js
+++ Client/src/app/Store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import rootRedcuer from "./rootRedcuer";
+import rootReducer from "./rootRedcuer";
 import { authApi } from "@/features/api/authApi";
 
 export const appStore = configureStore({
-    reducer: rootRedcuer,
-    middleware: (defaultMiddleware) => defaultMiddleware().concat(authApi.middleware)
+    reducer: rootReducer,
+    middleware:(defaultMiddlewarw) => defaultMiddlewarw().concat(authApi.middleware)
 });
