44681f5a9d0950bb61b604312df30dd30c3dc30c
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-09 00:18:38 +0600

    Update userController.js

--- Server/Controller/userController.js
+++ Server/Controller/userController.js
@@ -1,21 +1,22 @@
 import { User } from "../Models/userModel.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../Utils/generateToken.js";
+import { uploadMedia } from "../Utils/cloudinary.js";
 
 export const register = async (req, res) => {
     try {
-        const {name, email, password} = req.body;
-        if(!name || !email || !password){
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
             return res.status(400).json({
-                success:false,
+                success: false,
                 message: "All fields are required."
             })
         }
 
-        const user = await User.findOne({email});
+        const user = await User.findOne({ email });
         if (user) {
             return res.status(400).json({
-                success:false,
+                success: false,
                 message: "User already exist with this email."
             })
         }
@@ -23,76 +24,76 @@ export const register = async (req, res) => {
         await User.create({
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword
         });
         return res.status(201).json({
-            success:true,
-            message:"Account created successfully."
+            success: true,
+            message: "Account created successfully."
         })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Failed to register"
+            success: false,
+            message: "Failed to register"
         })
     }
 }
 
 export const login = async (req, res) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
         if (!email || !password) {
             return res.status(400).json({
-                success:false,
-                message:"All fields are required."
+                success: false,
+                message: "All fields are required."
             })
         }
-        const user = await User.findOne({email});
-        if(!user){
+        const user = await User.findOne({ email });
+        if (!user) {
             return res.status(400).json({
-                success:false,
-                message:"Incorrect email"
+                success: false,
+                message: "Incorrect email"
             })
         }
         const isPasswordMatch = await bcrypt.compare(password, user.password);
-        if(!isPasswordMatch){
+        if (!isPasswordMatch) {
             return res.status(400).json({
-                success:false,
-                message:"Incorrect password"
+                success: false,
+                message: "Incorrect password"
             })
         }
-        
+
         generateToken(res, user, `Welcome back ${user.name}`);
 
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Failed to register"
+            success: false,
+            message: "Failed to register"
         })
     }
 }
 
-export const logout = async (_,res) => {
+export const logout = async (_, res) => {
     try {
-        return res.status(200).cookie("token", "", {maxAge:0}).json({
-            message:"Logged out successfully.",
-            success:true
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged out successfully.",
+            success: true
         })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Failed to logout"
-        }) 
+            success: false,
+            message: "Failed to logout"
+        })
     }
 }
 
-export const getUserProfile = async (req,res) => {
+export const getUserProfile = async (req, res) => {
     try {
         const userId = req.id;
         const user = await User.findById(userId).select("-password");
-        if(!user){
+        if (!user) {
             return res.status(404).json({
                 message: "Profile not found",
                 success: false
@@ -105,8 +106,8 @@ export const getUserProfile = async (req,res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Failed to load user."
+            success: false,
+            message: "Failed to load user."
         })
     }
 }
@@ -114,22 +115,41 @@ export const getUserProfile = async (req,res) => {
 export const updateProfile = async (req, res) => {
     try {
         const userId = req.id;
-        const {name} = req.body;
+        const { name } = req.body;
         const profilePhoto = req.file;
 
         const user = await User.findById(userId);
-        if(!user){
+        if (!user) {
             return res.status(404).json({
                 message: "User not found",
                 success: false
             })
         }
-        
+
+        // extract public id of the old image from the url is it exists
+        if (user.photoUrl) {
+            const publicId = user.photoUrl.split("/").pop().split(".")[0]; // extract public id
+            deleteMediaFromCloudinary(publicId);
+        }
+
+        // upload new photo
+        const cloudResponse = await uploadMedia(profilePhoto.path);
+        const photoUrl = cloudResponse.secure_url;
+
+        const updatedData = { name, photoUrl };
+        const updatedUser = await User.findByIdAndUpdate(userId, updatedData, { new: true }).select("-password");
+
+        return res.status(200).json({
+            success: true,
+            user: updatedUser,
+            message: "Profile updated successful."
+        })
+
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            success:false,
-            message:"Failed to update profile."
+            success: false,
+            message: "Failed to update profile."
         })
     }
 }
