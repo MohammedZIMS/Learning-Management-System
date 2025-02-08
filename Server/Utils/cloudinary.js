a5e16cb7eada9dac6463395b3bb036255df98372
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-08 23:58:17 +0600

    Update cloudinary.js

--- Server/Utils/cloudinary.js
+++ Server/Utils/cloudinary.js
@@ -6,4 +6,32 @@ cloudinary.config({
     api_key: process.env.API_key,
     api_secret: process.env.API_secret,
     cloud_name: process.env.Cloud_NAME,
-})
+});
+
+export const uploadMedia = async (file) => {
+    try {
+        const uploadResponse = await cloudinary.uploader.upload(file, {
+            resource_type: "auto",
+        });
+        return uploadResponse;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteMediaFromCloudinary = async (publicId) => {
+    try {
+        await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteVideoFromCloudinary = async (publicId) => {
+    try {
+        await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+    } catch (error) {
+        console.log(error);
+
+    }
+};
