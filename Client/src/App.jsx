d7562838b171986b1a7563df48b9bc9719642418
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-13 22:50:37 +0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,22 +1,27 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import createBrowserRouter
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Login from './page/Login';
 import HeroSection from './page/student/HeroSection';
 import Courses from './page/student/Courses';
 import MyLearning from './page/student/MyLearning';
+import Profile from './page/student/Profile';
+import Sidebar from './page/Dashboard/Sidebar';
+import InstructorDashboard from './page/Dashboard/InstructorDashboard';
+import CourseTable from './page/admin/course/CourseTable';
+import AddCourse from './page/admin/course/AddCourse';
 
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <MainLayout />, // Ensures MainLayout wraps child routes
+    element: <MainLayout />,
     children: [
       {
         path: '/',
         element: (
           <>
             <HeroSection />
-            <Courses/> {/* Placeholder for future course component */}
+            <Courses/>
           </>
         ),
       },
@@ -28,12 +33,39 @@ const appRouter = createBrowserRouter([
         path: 'my-learning',
         element: <MyLearning/>,
       },
+      {
+        path: 'profile',
+        element: <Profile/>,
+      },
+      {
+        path: 'course',
+        element: <Courses/>,
+      },
+      {
+        path: 'dashboard',
+        element: <Sidebar />,
+        children: [
+          {
+            path: 'instructor-dashboard',
+            element: <InstructorDashboard />,
+          },
+          {
+            path: 'instructor-course',
+            element: <CourseTable/>,
+          },
+          {
+            path: 'instructor-course/create-course',
+            element: <AddCourse/>,
+          },
+          
+        ]
+      },
     ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={appRouter} />; // No unnecessary wrappers
+  return <RouterProvider router={appRouter} />;
 }
 
 export default App;
