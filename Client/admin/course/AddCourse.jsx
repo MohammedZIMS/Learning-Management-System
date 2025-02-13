29eb8734737b0af029bee4c01a835c0fa72f0fbd
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-13 18:13:13 +0600

    Create AddCourse.jsx

---
+++ Client/admin/course/AddCourse.jsx
@@ -0,0 +1,31 @@
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import React from 'react'
+
+const AddCourse = () => {
+  return (
+    <div className='flex-1 mx-10'>
+      <div className="mb-4">
+        <h1 className="font-bold text-xl">
+          Lets add course, add some basic course details for your new course
+        </h1>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aliquid optio officiis nihil modi fugit, praesentium eius excepturi reiciendis eos similique, sed vel, repellendus enim! Laborum modi ad eius harum!
+        </p>
+      </div>
+      <div className="space-4">
+        <div>
+          <Label>Title</Label>
+          <Input type="text" placeholder="Enter your Course Name"/>
+        </div>
+
+        <div>
+          <Label>Category</Label>
+        </div>
+      </div>
+        
+    </div>
+  )
+}
+
+export default AddCourse
