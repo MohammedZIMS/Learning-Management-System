0e9875abc2b53a160cbcaca296afdbf47c497748
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-23 23:48:58 +0600

    Update courseRoute.js

--- Server/Routes/courseRoute.js
+++ Server/Routes/courseRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import isAuthenticated from "../Middlewares/isAuthenticated.js";
-import { createCourse, editCourse, getCreatorCourses } from "../Controller/courseController.js";
 import upload from "../Utils/multer.js";
+import { createCourse, editCourse, getCreatorCourses } from "../Controller/courseController.js";
 
 const router = express.Router();
 
-router.route("/").post(isAuthenticated, createCourse);
-router.route("/").get(isAuthenticated, getCreatorCourses);
-router.route("/").post(isAuthenticated, upload.single("courseThumbnail"), editCourse);
+router.post("/", isAuthenticated, upload.single("courseThumbnail"), createCourse); // Correct field name
+router.get("/", isAuthenticated, getCreatorCourses);
+router.put("/:courseId", isAuthenticated, upload.single("courseThumbnail"), editCourse); // Correct method (PUT)
 
 export default router;
