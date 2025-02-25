ba3ce7f32362a25737c2c717c76200a657243316
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-02-25 23:49:14 +0600

    Update courseController.js

--- Server/Controller/courseController.js
+++ Server/Controller/courseController.js
@@ -133,35 +133,59 @@ export const getCourseById = async (req, res) => {
 }
 
 // create lecture Module
-export const createLectureModule = async (req,res) => {
+export const createLectureModule = async (req, res) => {
   try {
-      const {lectureTitle} = req.body;
-      const {courseId} = req.params;
+    const { lectureTitle } = req.body;
+    const { courseId } = req.params;
 
-      if(!lectureTitle || !courseId){
-          return res.status(400).json({
-              message:"Lecture title is required"
-          })
-      };
+    if (!lectureTitle || !courseId) {
+      return res.status(400).json({
+        message: "Lecture title is required"
+      })
+    };
 
-      // create lecture
-      const lecture = await Lecture.create({lectureTitle});
+    // create lecture
+    const lecture = await Lecture.create({ lectureTitle });
 
-      const course = await Course.findById(courseId);
-      if(course){
-          course.lectures.push(lecture._id);
-          await course.save();
-      }
+    const course = await Course.findById(courseId);
+    if (course) {
+      course.lectures.push(lecture._id);
+      await course.save();
+    }
 
-      return res.status(201).json({
-          lecture,
-          message:"Lecture created successfully."
-      });
+    return res.status(201).json({
+      lecture,
+      message: "Lecture module created successfully!"
+    });
 
   } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-          message:"Failed to create lecture"
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to create lecture module. Please try again."
+    })
+  }
+}
+
+// Get lecture Module
+export const getCouseLectureModule = async (req, res) => {
+  try {
+    const {courseId} = req.params;
+    const course = await Course.findById(courseId).populate("lectures");
+
+    if(!course){
+      return res.status(404).json({
+        message: "Course not found!"
       })
+    }
+
+    return res.status(200).json({
+      lectures: course.lectures
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to get lecture module. Please try again."
+    })
   }
 }
