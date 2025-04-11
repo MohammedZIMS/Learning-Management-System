48d3d9dbabbf5c43db9e6969a6d335ad1c807782
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-04-11 15:51:16 +0600

    Update lectureModel.js

--- Server/Models/lectureModel.js
+++ Server/Models/lectureModel.js
@@ -1,15 +1,29 @@
 import mongoose from "mongoose";
 
 const lectureSchema = new mongoose.Schema(
-    {
-        lectureTitle: {
-            type: String,
-            required: true,
-        },
-        videoUrl: { type: String },
-        publicId: { type: String },
-        isPreviewFree: { type: Boolean },
-    }, { timestamps: true });
-
+  {
+    lectureTitle: {
+      type: String,
+      required: true,
+    },
+    mediaType: {
+      type: String,
+      enum: ["video", "document"], // Defines allowed types
+    //   required: true,
+    },
+    mediaUrl: {
+      type: String,
+    //   required: true, // Ensures a file is always uploaded
+    },
+    publicId: {
+      type: String, // Cloudinary or other storage ID
+    },
+    isPreviewFree: {
+      type: Boolean,
+      default: false, // Defaults to not being free preview
+    },
+  },
+  { timestamps: true }
+);
 
 export const Lecture = mongoose.model("Lecture", lectureSchema);
