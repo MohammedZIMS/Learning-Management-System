b67d78913f90e85096a05744444b45d5f6619e46
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-05-01 21:48:50 +0600

    Update EditLecture.jsx

--- Client/src/page/admin/lectures/EditLecture.jsx
+++ Client/src/page/admin/lectures/EditLecture.jsx
@@ -2,16 +2,17 @@ import { Button } from '@/components/ui/button'
 import { ArrowLeft } from 'lucide-react'
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-import CreateLecture from './CreateLecture'
+import CreateLecture from './LectureTab'
 
 const EditLecture = () => {
     const params = useParams();
     const courseId = params.courseId;
+    const moduleId = params.moduleId;
     return (
         <div>
             <div className='flex items-center justify-between mb-5'>
                 <div className="flex items-center gap-2">
-                    <Link to={`/dashboard/instructor-course/${courseId}/lecture-module`}>
+                    <Link to={`/dashboard/instructor-course/${courseId}/modules/${moduleId}/lecture`}>
                         <Button size='icon' variant='outline' className='rounded-full'>
                             <ArrowLeft size={16} />
                         </Button>
@@ -19,7 +20,7 @@ const EditLecture = () => {
                     <h1 className="font-bold text-xl">Update Your Lecture</h1>
                 </div>
             </div>
-            <CreateLecture />
+            <CreateLecture/>
         </div>
     )
 }
