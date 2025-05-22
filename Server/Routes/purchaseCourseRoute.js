3a3b9831da4ae4bf0c39ce426512cc846c4833d3
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-05-22 20:10:27 +0600

    Update purchaseCourseRoute.js

--- Server/Routes/purchaseCourseRoute.js
+++ Server/Routes/purchaseCourseRoute.js
@@ -2,19 +2,18 @@ import express from 'express';
 import { createCheckoutSession, getAllPurchasedCourses, getCourseDetailsWithPurchaseStatus, stripeWebhook } from '../Controller/coursePurchaseController.js';
 import  isAuthenticated  from "../Middlewares/isAuthenticated.js";
 
-
 const router = express.Router();
 
 // Secure checkout route
 router.post('/checkout/create-checkout-session', isAuthenticated, createCheckoutSession);
 
 // Stripe requires raw body for signature verification
-router.post('/webhook', express.raw({ type: 'application/json' }), stripeWebhook);
+router.route("/webhook").post(express.raw({type:"application/json"}), stripeWebhook);
 
 // Route to get course details with purchase status
 router.get('/course/:courseId/detail-with-status',isAuthenticated, getCourseDetailsWithPurchaseStatus);
 
 // Route to get all purchased courses
-router.get('/',isAuthenticated, getAllPurchasedCourses);
+router.get('',isAuthenticated, getAllPurchasedCourses);
 
 export default router;
