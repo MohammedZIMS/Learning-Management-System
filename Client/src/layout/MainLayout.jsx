4bfbbaf983a9e5e382784db890e2a55bf537ce91
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-05-23 11:21:11 +0600

    Update MainLayout.jsx

--- Client/src/layout/MainLayout.jsx
+++ Client/src/layout/MainLayout.jsx
@@ -6,9 +6,8 @@ import { Outlet } from 'react-router-dom'
 const MainLayout = () => {
   return (
     <div>
-        <Navbar/>
-
-        <div>
+        <Navbar className='flex flex-col min-h-screen'/>
+        <div className='flex-1 mt-10'>
             <Outlet/>
         </div>
     </div>
