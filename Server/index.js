ce96e562fd026d53624fa6ac065372e217deb0fe
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2025-05-25 21:47:48 +0600

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -32,6 +32,45 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use('/api/v1/purchase', purchaseCourseRoute);
 app.use('/api/v1/progress', courseProgressRoute);
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB  from "./Database/database.js";
+import userRoute from "./Routes/userRoute.js";
+import courseRoute from "./Routes/courseRoute.js";
+import mediaRoute from "./Routes/mediaRoute.js";
+import purchaseCourseRoute from "./Routes/purchaseCourseRoute.js";
+import courseProgressRoute from "./Routes/courseProgressRoute.js";
+
+dotenv.config();
+
+// call database connection
+connectDB({});
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}));
+
+// apis
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use('/api/v1/purchase', purchaseCourseRoute);
+app.use('/api/v1/progress', courseProgressRoute);
 
 
 app.listen(PORT, () => {
